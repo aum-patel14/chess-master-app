c64e53d9979f790b7afd23c2b020216edfe39f7b
Aum Patel <aumpatel14@gmail.com>
2026-06-22 10:53:45 +0530

    Fix legacy service worker caching issue by adding self-destructing SW and client-side cleanup script

---
+++ chessmaster-pro/public/sw.js
@@ -0,0 +1,18 @@
+// Self-destructing Service Worker to clean up old PWA cache/service worker registrations
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    self.registration.unregister()
+      .then(() => self.clients.matchAll())
+      .then((clients) => {
+        clients.forEach((client) => {
+          if (client.url && 'navigate' in client) {
+            client.navigate(client.url);
+          }
+        });
+      })
+  );
+});
